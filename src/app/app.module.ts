82a805dd6971b9cb695bd3bfc41dcb001f031256
TUNE <gytune74@gmail.com>
2017-07-07 19:37:55 +0200

    refactoring add routing and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule}   from '@angular/forms'; // <-- NgModel lives here
-import {HeroDetailComponent} from './hero-detail.component';
 
-import { AppComponent }  from './app.component';
+import {HeroDetailComponent} from './hero-detail.component';
+import {HeroesComponent}  from './heroes.component';
+import {HeroService} from './hero.service';
+import {AppComponent} from './app.component';
+import {DashboardComponent} from './dashboard.component';
+import {AppRoutingModule}     from './app-routing.module';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HeroDetailComponent],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  declarations: [AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent],
+  bootstrap: [AppComponent],
+  providers: [HeroService]
 })
+
 export class AppModule { }
---
+++ src/app/hero.service.ts
@@ -0,0 +1,26 @@
+/**
+ * Created by TUNE on 2017.07.06..
+ */
+import {Injectable} from '@angular/core';
+
+import {Hero} from './hero';
+import {HEROES} from './mock-heroes';
+
+@Injectable()
+export class HeroService {
+  getHeroes(): Promise<Hero[]> {
+    return Promise.resolve(HEROES);
+  }
+
+  getHeroesSlowly(): Promise<Hero[]> {
+    return new Promise(resolve => {
+      // Simulate server latency with 2 second delay
+      setTimeout(() => resolve(this.getHeroes()), 2000);
+    });
+  }
+
+  getHero(id: number): Promise<Hero> {
+    return this.getHeroes()
+      .then(heroes => heroes.find(hero => hero.id === id));
+  }
+}
