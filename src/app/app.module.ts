eaaff0025f8291ad93e2a79f8908f4accc70ea56
TUNE <gytune74@gmail.com>
2017-07-10 21:01:34 +0200

    Adding service and data manipulation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule}   from '@angular/forms'; // <-- NgModel lives here
+import { HttpModule }    from '@angular/http';
 
 import {HeroDetailComponent} from './hero-detail.component';
 import {HeroesComponent}  from './heroes.component';
@@ -9,8 +10,18 @@ import {AppComponent} from './app.component';
 import {DashboardComponent} from './dashboard.component';
 import {AppRoutingModule}     from './app-routing.module';
 
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 @NgModule({
-  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService)
+  ],
   declarations: [AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent],
   bootstrap: [AppComponent],
   providers: [HeroService]
